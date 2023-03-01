567de5abfc4e790769d964303e6cfb50090f1879
sunysa8 <sunysa8@naver.com>
2023-03-01 19:17:55 +0900

    week5_윤선영

---
+++ 03.순차적자료구조/4828_min_max.py
@@ -0,0 +1,15 @@
+
+T = int(input())
+
+# 완전 기초 max, min 함수 사용
+# for tc in range(1, T + 1):
+#     N = int(input())
+#     lst = list(map(int, input().split()))
+#     result = max(lst) - min(lst)
+#     print(f'#{tc} {result}')
+
+# 버블소트
+for tc in range(1, T + 1):
+    N = int(input())
+    lst = list(map(int, input().split()))
+
